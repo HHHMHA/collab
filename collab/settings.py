db0aa6eeac572b3f9d28e251fe487cf7b22648ad
Al-Hasan Saleh <alhasan.saleh@kuwaitnet.com>
2022-05-24 21:52:24 +0300

    Fix login and logout redirect url

--- collab/settings.py
+++ collab/settings.py
@@ -132,5 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
 
-LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+LOGIN_REDIRECT_URL = "task:home"
+LOGOUT_REDIRECT_URL = "task:home"
