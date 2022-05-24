f3f945456372c8b5c04cb9bb04b83e3c2441e04e
Al-Hasan Saleh <alhasan.saleh@kuwaitnet.com>
2022-05-24 18:58:00 +0300

    Style home template

--- collab/urls.py
+++ collab/urls.py
@@ -14,12 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import path, include
 from django.views.generic import TemplateView
+
+
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = 'home.html'
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("tasks/", include("task.urls")),
-    path("", TemplateView.as_view(template_name='home.html'), name='home'),
+    path("", HomeView.as_view(), name='home'),
 ]
